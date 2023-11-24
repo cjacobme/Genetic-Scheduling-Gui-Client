59ec962789d542b758121ddf4f7876c3419e22cb
cjacobme <cjacobme@aol.com>
2023-11-24 22:56:37 +0100

    first attempt for RESTful requests

--- src/main/java/cj/software/genetics/schedule/client/util/BeanProducer.java
+++ src/main/java/cj/software/genetics/schedule/client/util/BeanProducer.java
@@ -1,14 +1,33 @@
 package cj.software.genetics.schedule.client.util;
 
+import cj.software.genetics.schedule.client.entity.configuration.ConfigurationHolder;
+import cj.software.genetics.schedule.client.entity.configuration.Server;
 import javafx.util.converter.NumberStringConverter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+import java.net.URL;
 
 @Component
 public class BeanProducer {
 
+    @Autowired
+    private ConfigurationHolder configurationHolder;
+
     @Bean
     public NumberStringConverter numberStringConverter() {
         return new NumberStringConverter();
+    }
+
+    @Bean
+    public WebClient webClient() {
+        Server server = configurationHolder.getServer();
+        URL url = server.getUrl();
+        WebClient result = WebClient.builder()
+                .baseUrl(url.toString())
+                .build();
+        return result;
     }
 }
